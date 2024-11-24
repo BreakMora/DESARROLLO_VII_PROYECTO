07fe389a879424e20078709708c21976f4946abd
BreakMora <edgar07mora17@gmail.com>
2024-11-24 13:58:20 -0500

    Proyecto: Fix bug ObtenerRerservacionesAdmin.php.

--- app/controllers/ObtenerReservacionesAdmin.php
+++ app/controllers/ObtenerReservacionesAdmin.php
@@ -5,7 +5,7 @@ require_once(__DIR__ . '/../models/Reservaciones.php');
 require_once(__DIR__ . '/../models/Clientes.php');
 
 if (isset($_SESSION['id'])) {
-    $id_usuario = $_SESSION['id'];
+    
 } else {
     // Redirige al usuario a la página de inicio de sesión si no ha iniciado sesión
     header("Location: ../../public/Login.php");
@@ -18,24 +18,27 @@ $resultado = $reservacion_controller->obtenerReservaciones();
 $reservaciones = [];
 
 // Obtener el nombre del cliente (usuario) basado en el id_usuario
-$cliente_resultado = $datos_usuario->getNombreCliente($id_usuario);
-$nombre_cliente = '';
-$apellido_cliente = '';
+$cliente_resultado = $datos_usuario->getDatosClientes();
+$cliente = [];
 
 if ($cliente_resultado->num_rows > 0) {
-    $cliente = $cliente_resultado->fetch_assoc();
-    $nombre_cliente = $cliente['nombre']; 
-    $apellido_cliente = $cliente['apellido'];
+    while ($cliente = $cliente_resultado->fetch_assoc()) {
+        $clientes[$cliente['cliente_id']] = [
+            'nombre' => $cliente['nombre'],
+            'apellido' => $cliente['apellido']
+        ];
+    }
 }
 
 if ($resultado->num_rows > 0) {
     while ($fila = $resultado->fetch_assoc()) {
         $habitacion_id = $fila['habitacion_id'];
-        $fecha_reservacion = $fila['fecha_reservacion']; // Obtener la fecha de la reservación
+        $fecha_reservacion = $fila['fecha_reservacion'];
         $reservacion_id = $fila['reservacion_id'];
         $fecha_entrada = $fila['fecha_entrada'];
         $fecha_salida = $fila['fecha_salida'];
         $costo = $fila['costo'];
+        $cliente_id = $fila['cliente_id'];
 
         // Obtener los detalles de la habitación
         $habitacion_controller = new Habitaciones($conn);
@@ -49,8 +52,15 @@ if ($resultado->num_rows > 0) {
             $habitacion['fecha_entrada'] = $fecha_entrada;
             $habitacion['fecha_salida'] = $fecha_salida;
             $habitacion['costo'] = $costo;
-            $habitacion['nombre_cliente'] = $nombre_cliente;
-            $habitacion['apellido_cliente'] = $apellido_cliente;
+            
+            if (isset($clientes[$cliente_id])) {
+                $habitacion['nombre_cliente'] = $clientes[$cliente_id]['nombre'];
+                $habitacion['apellido_cliente'] = $clientes[$cliente_id]['apellido'];
+            } else {
+                $habitacion['nombre_cliente'] = 'Desconocido';
+                $habitacion['apellido_cliente'] = '';
+            }
+
             $reservaciones[] = $habitacion;
         }
     }
